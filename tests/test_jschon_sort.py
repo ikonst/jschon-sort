584841fc97418b99113f1ddf7c79fbaa9087d1f6
Ilya Konstantinov <ilya.konstantinov@gmail.com>
2021-12-07 10:36:21 -0500

    Add failing test

--- tests/test_jschon_sort.py
+++ tests/test_jschon_sort.py
@@ -62,3 +62,35 @@ def test_sort_doc_by_schema(schema_version: str) -> None:
     assert actual is not doc
     assert json.dumps(doc) == doc_str, "ensure doc is not modified in place"
     assert json.dumps(actual) == '{"ranges": [{"BBB": 42, "AAA": 42, "start": 10, "end": 20}]}'
+
+
+def test_sort_doc_by_schema__local_ref() -> None:
+    # Arrange
+    doc_str = '{"foo": {"end": 20, "start": 10}}'
+    doc = json.loads(doc_str)
+
+    schema = {
+        "$schema": "https://json-schema.org/draft/2020-12/schema",
+        "type": "object",
+        "$defs": {
+            "range": {
+                "type": "object",
+                "properties": {
+                    "start": {"type": "number"},
+                    "end": {"type": "number"},
+                },
+                "required": ["start", "end"],
+                "additionalProperties": False,
+            },
+        },
+        "additionalProperties": {
+            "$ref": "#/$defs/range",
+        },
+    }
+
+    # Act
+    actual = sort_doc_by_schema(doc_data=doc, schema_data=schema)
+
+    # Assert
+    assert actual is not doc
+    assert json.dumps(actual) == '{"foo": {"start": 10, "end": 20}}'
